cdbd39040a14fcaccc30772aee10045a9594a97d
BasyninNikita <32190908+BasyninNikita@users.noreply.github.com>
2017-11-17 15:20:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <utility>
 #include <sstream>
 using namespace std;
 void vyvod(int *array,unsigned int size)
@@ -13,60 +12,57 @@ void vyvod(int *array,unsigned int size)
 bool vvod(int *array,unsigned int size)
 {
     string stroka;
-    getline(cin,stroka);
+    getline(cin,stroka);	
     istringstream stream(stroka);
     for(unsigned int i=0;i<size;i++)
     {
         if(!(stream>>array[i])){
             cout<<"nepolnuy massiv";
             return false;
-        }   
+        }   	
     }
     return true;
 } 
-void merge(int *array,int left,int right,int razd)
-{	
-	int *array1=new int[right-left];
-	int i=0;int l=left;int r=razd+1;
-	while((l<=razd)&&(r<=right))
-	{
-		if(array[l]>array[r])
-		{
-			array1[i]=array[l];
-			l++;i++;
-		}
-		else if(array[l]<array[r])
-		{
-			array1[i]=array[r];
-			r++;i++;
-		}
-	}
-	while (l<=razd)
-	{
-		array1[i]=array[l];
-		l++;i++;
-	}
-	while (r<=right)
-	{
-		array1[i]=array[r];
-		r++;i++;
-	}
-	for(i=0;i<l+r;i++)
-	{
-		array[i]=array1[i];
-	}
-	delete[]array1;
+void mergesort(int * array,int left, int right)
+{
+    if (left < right)
+    {
+        int razd = (left + right)/2;
+        mergesort(array, left, razd);
+        mergesort(array, razd+1, right);
+        int * array1 = new int[right - left +1];
+        int i = 0;
+        int l = left;
+        int r = razd + 1;
+        while(l <= razd && r <= right)
+        {
+             if(array[l] < array[r])
+             {
+                array1[i] = array[l++];
+             }
+             else
+             {
+                 array1	[i] = array[r++];
+             }
+             i++;
+        }
+        while( r <= right )
+        {
+             array1[ i ] = array[ r++ ];
+             i++;
+        }
+        while( l <= razd )
+        {
+              array1[ i ] = array[ l++ ];
+              i++;
+        }
+        for( i = 0; i < right-left+1; i++ )
+        {
+           array[left+i] = array1[i];
+        }
+        delete [] array1;
+    }
 }
-long mergesort(int *array,int left,int right) { 
-	long razd;                   
-	if (left < right)	
-	return          
-    	razd = (left + right)/2;
-    	mergesort(array, left, razd);       
-    	mergesort(array,razd	, right);  
-    	merge(array, left, right, razd);    
-}
-
 int main(int argc, char** argv) {
 	unsigned int size;
 	string stroka;
